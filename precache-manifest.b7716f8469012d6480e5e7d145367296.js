2a270da2192bc42d2bdbfe2c318fa62e1367df09
Max Ulshin <max.ulshin.max@istat.com.ua>
2020-09-18 18:07:37 +0300

    Updates

---
+++ precache-manifest.b7716f8469012d6480e5e7d145367296.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2e7c0e830e3efab81a026687faf4be8",
+    "url": "/Nedo_Site_To_deploy/index.html"
+  },
+  {
+    "revision": "6a6da5f2f43fe2bfa7dc",
+    "url": "/Nedo_Site_To_deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "221f913cadd24b0803f2",
+    "url": "/Nedo_Site_To_deploy/static/js/2.a756137c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nedo_Site_To_deploy/static/js/2.a756137c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a6da5f2f43fe2bfa7dc",
+    "url": "/Nedo_Site_To_deploy/static/js/main.16dc1fbf.chunk.js"
+  },
+  {
+    "revision": "ad97c3b135cd8f1b3b32",
+    "url": "/Nedo_Site_To_deploy/static/js/runtime-main.9ff24e31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Nedo_Site_To_deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
